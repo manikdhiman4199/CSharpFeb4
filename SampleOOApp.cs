d334ecde281c716af15b1273a77ec9455b85d792
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-02-04 08:45:52 -0500

    2
    2

--- SampleOOApp.cs
+++ SampleOOApp.cs
@@ -12,15 +12,36 @@ namespace ConsoleApp2
         public static void DoCompetition()
         {
             Lion L1 = new Lion();
+            Tiger T1 = new Tiger();
+            L1.setName("Leo");
+            T1.setName("Tiggy");
+            Console.WriteLine("Lion's name is {0}", L1.getName());
+            Console.WriteLine("Tiger's name is {0}", T1.getName());
         }
 
     }
-
-    class Lion
+    class Feline
     {
-        String Name;
-        int Strength() {
-            return 0;
-        }
+            String Name;
+            int Strength() {
+                        return 0;
+            }
+            public String getName()
+            {
+                return this.Name;
+            }
+            public void setName(String aName)
+            {   //variable scoping:
+                this.Name = aName;
+            }
+    }
+    class Lion:Feline
+    {
+        
+    }
+
+    class Tiger:Feline
+    {
+        
     }
 }
