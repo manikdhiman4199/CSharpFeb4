9482ff1de9c9d7336cb7948fc928e21a5d0d0829
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-02-04 08:31:14 -0500

    1
    1

---
+++ SampleOOApp.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class JUNGLE
+    {
+        static void Main(string[] args)
+        {
+            DoCompetition();
+        }
+
+        public static void DoCompetition()
+        {
+            Lion L1 = new Lion();
+        }
+
+    }
+
+    class Lion
+    {
+        String Name;
+        int Strength() {
+            return 0;
+        }
+    }
+}
